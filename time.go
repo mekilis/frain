e71dfeba689c2a2a74bb0d98a6f5581a448507e6
mekilis <st.nonso@gmail.com>
2019-04-26 13:11:28 +0100

    implemented the TimeAgo function

--- time.go
+++ time.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
+var (
+	// ErrFutureTime is an error that is thrown when the reference time is greater than
+	// the supposedly future time to check
+	ErrFutureTime = errors.New("reference time argument is in the future")
+)
+
+// Clock basically stores the essential time.Time information excluding milliseconds
 type Clock struct {
 	Second int
 	Minute int
@@ -14,11 +23,69 @@ type Clock struct {
 	Year  int
 }
 
-func TimeAgo(t time.Time) (string, error) {
+// TimeAgo takes a time.Time construct from the past and calculates the difference between it (t) and
+// the reference time (ref) and then appending 'ago' at the end
+func TimeAgo(ref, t time.Time) (string, error) {
+	if ref.After(t) {
+		return "", ErrFutureTime
+	}
 
-	return "", nil
+	elapsed := TimeDiff(t, ref)
+	unit := "years"
+	if elapsed.Year == 0 {
+		unit = "months"
+
+		if elapsed.Month == 0 {
+			unit = "days"
+
+			if elapsed.Day == 0 {
+				unit = "hours"
+
+				if elapsed.Hour == 0 {
+					unit = "minutes"
+
+					if elapsed.Minute == 0 {
+						unit = "seconds"
+
+						if elapsed.Second == 1 {
+							unit = "second"
+						}
+
+						return fmt.Sprintf("%d %s ago", elapsed.Second, unit), nil
+					}
+					if elapsed.Minute == 1 {
+						unit = "minute"
+					}
+
+					return fmt.Sprintf("%d %s ago", elapsed.Minute, unit), nil
+				}
+				if elapsed.Hour == 1 {
+					unit = "hour"
+				}
+
+				return fmt.Sprintf("%d %s ago", elapsed.Hour, unit), nil
+			}
+			if elapsed.Day == 1 {
+				unit = "day"
+			}
+
+			return fmt.Sprintf("%d %s ago", elapsed.Day, unit), nil
+		}
+		if elapsed.Month == 1 {
+			unit = "month"
+		}
+
+		return fmt.Sprintf("%d %s ago", elapsed.Month, unit), nil
+
+	}
+	if elapsed.Year == 1 {
+		unit = "year"
+	}
+
+	return fmt.Sprintf("%d %s ago", elapsed.Year, unit), nil
 }
 
+// TimeDiff finds the time elapsed between two time.Time types
 func TimeDiff(t1, t2 time.Time) Clock {
 	// set same location
 	if t1.Location() != t2.Location() {
--- time_test.go
+++ time_test.go
@@ -21,3 +21,46 @@ func TestTimeDiff(t *testing.T) {
 		}
 	}
 }
+
+func TestTimeAgo(t *testing.T) {
+	tests := []struct {
+		createdAt   time.Time
+		updatedAt   time.Time
+		wantElapsed string
+		wantErr     error
+	}{
+		{
+			time.Date(2019, 8, 17, 0, 5, 23, 0, time.UTC),
+			time.Date(2019, 8, 18, 12, 19, 47, 0, time.UTC),
+			"1 day ago",
+			nil,
+		},
+		{
+			time.Date(2019, 8, 18, 12, 19, 47, 0, time.UTC),
+			time.Date(2019, 8, 17, 0, 5, 23, 0, time.UTC),
+			"",
+			ErrFutureTime,
+		},
+		{
+			time.Date(2002, 11, 9, 0, 5, 53, 0, time.UTC),
+			time.Date(2019, 11, 9, 0, 1, 24, 0, time.UTC),
+			"17 years ago",
+			nil,
+		},
+		{
+			time.Date(2020, 7, 23, 6, 5, 53, 0, time.UTC),
+			time.Date(2020, 7, 23, 6, 7, 23, 0, time.UTC),
+			"1 minute ago",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		elapsed, err := TimeAgo(tt.createdAt, tt.updatedAt)
+		if elapsed != tt.wantElapsed {
+			t.Errorf("expected %v got %v", tt.wantElapsed, elapsed)
+		} else if err != tt.wantErr {
+			t.Errorf("expected %v got %v", tt.wantErr, err)
+		}
+	}
+}

2f7d8078303d611b821670a0a7b46a5dc400bb8e
mekilis <st.nonso@gmail.com>
2019-05-05 00:12:00 +0100

    updated query to fetch incidents

--- server.go
+++ server.go
@@ -3,8 +3,10 @@ package frain
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Data struct {
@@ -55,11 +57,11 @@ func Services() (map[string][]Service, error) {
 	return queryMap, nil
 }
 
-func GetServiceFor(name string) (*Service, error) {
-	query := bytes.NewBuffer([]byte("{ \"query\": \"{getService(name:" + name +
-		") {id, name, statusPageUrl," +
-		"provider, indicator, isActive, createdAt, updatedAt, components{id, name, status, " +
-		"description}, incidents(startTime:\\\"2019-05-01\\\", endTime:\\\"2019-05-05\\\"){id, name,impact, status, isActive, createdAt, shortlink, updatedAt," +
+func GetServiceFor(name string, startTime, endTime time.Time) (*Service, error) {
+	query := bytes.NewBuffer([]byte("{ \"query\": \"{getService(name:" + name + ")" +
+		"{id, name, statusPageUrl, provider, indicator, isActive, createdAt, updatedAt, components{id, name, status, description}," +
+		"incidents(startTime:\\\"" + parseDate(&startTime) + "\\\", endTime:\\\"" +
+		parseDate(&endTime) + "\\\"){id, name,impact, status, isActive, createdAt, shortlink, updatedAt," +
 		"incidentUpdates{id, body, status, createdAt, updatedAt}}}}\"}"))
 	// TODO: fix resolvedAt issues
 	host := os.Getenv("FRAIN_HOST")
@@ -81,4 +83,8 @@ func GetServiceFor(name string) (*Service, error) {
 	}
 
 	return &result.Service, nil
+}
+
+func parseDate(t *time.Time) string {
+	return fmt.Sprintf("%04d-%02d-%02d", t.Year(), int(t.Month()), t.Day())
 }

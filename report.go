b89f8c4de5c4f743c5cfea3b9d4a065963dcff14
mekilis <st.nonso@gmail.com>
2019-05-05 00:11:07 +0100

    refractored report.go

--- report.go
+++ report.go
@@ -10,9 +10,8 @@ import (
 
 // Report is an interface implemented by types that generates report in different formats.
 type Report interface {
-	Incident(*Incident) error
-	Service(*Service) error
-	All() error
+	Incidents(bool)
+	All(bool)
 }
 
 // Text is a construct to display the page information in text
@@ -20,79 +19,108 @@ type Text struct {
 	Data *Page
 }
 
-// Incident implements the Report interface
-func (t Text) Incident(i *Incident) error {
-	return nil
-}
-
-// Service implements the Report interface
-func (t Text) Service(s *Service) error {
-	return nil
+// Incidents implements the Report interface
+func (t Text) Incidents(quiet bool) {
+	w := new(tabwriter.Writer)
+	printIncidents(w, t.Data.Service.Incidents)
 }
 
 // All implements the Report interface
-func (t Text) All() error {
+func (t Text) All(quiet bool) {
 	w := new(tabwriter.Writer)
 	service := t.Data.Service
-	fmt.Printf("----------------------------------\n")
 
 	name := strings.Title(service.Name)
 	titleService := fmt.Sprintf("%s Services", name)
-	colComponents := "\nService Name\tStatus\n-------------\t-------"
+	if quiet {
+		summarize(titleService, service.Components, service.Incidents)
+		return
+	}
 
-	w.Init(os.Stdout, 0, 8, 2, '\t', tabwriter.AlignRight)
+	fmt.Printf("----------------------------------\n")
 	fmt.Println(titleService)
+	printComponents(w, service.Components)
+	printIncidents(w, service.Incidents)
+}
+
+func summarize(title string, components []Component, incidents []Incident) error {
+	op := 0
+	numC := 0
+	for _, c := range components {
+		if c.Status == "operational" {
+			op++
+		}
+		numC++
+	}
+
+	fmt.Printf("%s: %d/%d component(s) are operational. %d incident(s) reported.\n",
+		title,
+		op,
+		numC,
+		len(incidents),
+	)
+
+	return nil
+}
+
+func printComponents(w *tabwriter.Writer, comps []Component) {
+	colComponents := "\nComponent Name\tStatus\n-------------\t-------"
+	w.Init(os.Stdout, 0, 8, 2, '\t', tabwriter.AlignRight)
+
 	fmt.Fprintln(w, colComponents)
-	for _, comp := range service.Components {
-		fmt.Fprintln(w, fmt.Sprintf("%s\t%s", strings.Title(comp.Name), strings.Title(comp.Status)))
+	for _, c := range comps {
+		fmt.Fprintln(w, fmt.Sprintf("%s\t%s", strings.Title(c.Name), strings.Title(c.Status)))
 	}
 	w.Flush()
-	if len(service.Components) == 0 {
-		fmt.Println("No service reports")
-	}
 
+	if len(comps) == 0 {
+		fmt.Println("No component reports")
+	}
+}
+
+func printIncidents(w *tabwriter.Writer, inc []Incident) {
 	colIncidents := "Date\tTime\tStatus\tDescription\tUpdated\n----------" +
 		"\t---------\t----------\t-------------\t------------"
-	n := len(service.Incidents)
 
 	w.Init(os.Stdout, 0, 8, 2, '\t', tabwriter.AlignRight)
 	fmt.Println("\nIncident History\n-----------------")
 
-	if n > 0 {
-		fmt.Fprintln(w, colIncidents)
-		for _, i := range service.Incidents {
-			// update using the last incidentUpdate ?? uncertain
-			if n := len(i.IncidentUpdates); n > 0 {
-				i.Status = i.IncidentUpdates[0].Status
-				i.UpdatedAt = i.IncidentUpdates[0].UpdatedAt
-			}
+	fmt.Fprintln(w, colIncidents)
 
-			elapsed, _ := TimeAgo(i.UpdatedAt, time.Now())
-			if elapsed == "0 seconds ago" {
-				elapsed = "     -"
-			}
-
-			fmt.Fprintln(
-				w,
-				fmt.Sprintf("%s %d %d\t%d:%d:%d\t%s\t%s\t%s",
-					i.CreatedAt.Month(),
-					i.CreatedAt.Day(),
-					i.CreatedAt.Year(),
-
-					i.CreatedAt.Hour(),
-					i.CreatedAt.Minute(),
-					i.CreatedAt.Second(),
-
-					i.Status,
-					i.Impact,
-					elapsed, //TODO: call TimeAgo() here
-				),
-			)
-		}
-	} else {
+	n := len(inc)
+	if n == 0 {
 		fmt.Println("No incident reports")
+		return
+	}
+
+	for _, i := range inc {
+		// update using the last incidentUpdate ?? uncertain
+		if nIU := len(i.IncidentUpdates); nIU > 0 {
+			i.Status = i.IncidentUpdates[0].Status
+			i.UpdatedAt = i.IncidentUpdates[0].UpdatedAt
+		}
+
+		elapsed, _ := TimeAgo(i.UpdatedAt, time.Now())
+		if elapsed == "0 seconds ago" {
+			elapsed = "     -"
+		}
+
+		fmt.Fprintln(
+			w,
+			fmt.Sprintf("%s %d %d\t%d:%d:%d\t%s\t%s\t%s",
+				i.CreatedAt.Month(),
+				i.CreatedAt.Day(),
+				i.CreatedAt.Year(),
+
+				i.CreatedAt.Hour(),
+				i.CreatedAt.Minute(),
+				i.CreatedAt.Second(),
+
+				i.Status,
+				i.Impact,
+				elapsed,
+			),
+		)
 	}
 	w.Flush()
-
-	return nil
 }

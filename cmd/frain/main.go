1e1d5c19879df6dab2c3fa6f2c91010d464394b2
mekilis <st.nonso@gmail.com>
2019-05-27 13:12:29 +0100

    refracted main

--- cmd/frain/main.go
+++ cmd/frain/main.go
@@ -33,6 +33,10 @@ var (
 	versionFlag = flag.Bool("version", false, version)
 
 	buildVersion string
+
+	subCommands = map[string]bool{
+		"incidents": true,
+	}
 )
 
 func init() {
@@ -57,7 +61,7 @@ func init() {
 			green("\n\t-q <service>,\t--quiet <service>\t"), "Displays just the summary for specified service",
 			green("\n\t-v,\t--version\t"), "Displays the current version of this program\n",
 			yellow("\nArgs:"),
-			"\t<service>\n\t<service> ", green("incidents"),
+			"\n\t<service>\n\t<service> ", green("incidents"),
 			"\n\t<service> ", green("incidents <start time> <end time>\n\n"),
 			"Note that both start and end times have the format YYYY-MM-DD\n",
 			yellow("\nExamples:"),
@@ -73,136 +77,78 @@ func init() {
 
 func main() {
 	flag.Parse()
-	versionInfo()
+	setupVerInfo()
+	parseFlagOptions()
 
-	if *versionFlag {
-		frain.Init()
-		os.Exit(0)
-	}
-
-	if *helpFlag {
-		frain.Init()
-		flag.Usage()
-		os.Exit(0)
-	}
-
-	if *listFlag {
-		frain.Init()
-		listServices()
-		os.Exit(0)
-	}
-
-	if len(*configFlag) != 0 {
-		fmt.Println("-c flag set. this feature has not yet been implemented. please check again later.")
-		os.Exit(0)
-	}
-
-	format := strings.ToLower(*formatFlag)
-	if format != "txt" && format != "json" && format != "xml" {
-		fmt.Printf("Error: bad format specified '%s'\n", format)
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	osArgsLen := len(os.Args)
 	flagArgs := flag.Args()
-	flagArgsLen := len(flagArgs)
-	if osArgsLen < 2 || flagArgsLen == 0 {
-		fmt.Println("Error: no service specified.")
-		flag.Usage()
-		os.Exit(1)
+	format := strings.ToLower(*formatFlag)
+
+	if format != "txt" && format != "json" && format != "xml" {
+		fmt.Printf("frain: bad format specified '%s' (\"frain help\" for help)\n", format)
+		exit()
 	}
 
-	startTime, _ := time.Parse("2006-01-02", "1970-01-01") // iso layout
-	endTime := time.Now()
-
-	var err error
-	if flagArgsLen > 1 {
-		if flagArgs[1] != "incidents" {
-			fmt.Printf("unknown query specified for %s: '%s'\n", flagArgs[0], flagArgs[1])
-			os.Exit(2)
-		}
-		if flagArgsLen > 2 {
-			sTime, err := frain.CleanTimeArg(flagArgs[2])
-			if err != nil {
-				fmt.Println("start time error.", err)
-				os.Exit(4)
-			}
-			startTime, err = time.Parse("2006-01-02", sTime)
-			if err != nil {
-				fmt.Println("bad format specified for start time:", sTime)
-				os.Exit(4)
-			}
-		}
-		if flagArgsLen > 3 {
-			eTime, err := frain.CleanTimeArg(flagArgs[3])
-			if err != nil {
-				fmt.Println("end time error.", err)
-				os.Exit(4)
-			}
-			endTime, err = time.Parse("2006-01-02", eTime)
-			if err != nil {
-				fmt.Println("bad format specified for end time:", eTime)
-				os.Exit(4)
-			}
-		}
-
-		// other subcommands
+	if len(os.Args) < 2 || len(flagArgs) == 0 {
+		fmt.Println("frain: no service specified (\"frain help\" for help)")
+		exit()
 	}
 
-	var page frain.Page
-	name := strings.ToLower(flagArgs[0])
-	page.Name = name
-
-	var c = make(chan int)
-	go progress(c)
-
-	service, err := frain.GetService(name, startTime, endTime)
-	c <- 1
-	clear()
+	subCommand, startTime, endTime, err := parseFlagParams(flagArgs)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+		fmt.Println("frain:", err, "(\"frain help\" for help)")
+		exit()
 	}
 
-	if strings.ToLower(service.Name) != name {
-		fmt.Printf("Error: '%s' is not a recognized service on frain. Try running 'frain --list'.\n", name)
-		os.Exit(2)
+	name := strings.ToLower(flagArgs[0])
+	report, err := getReport(name, format, startTime, endTime)
+	if err != nil {
+		fmt.Println("frain:", err)
+		exit()
 	}
 
-	page.Service = service
-
-	var report frain.Report
-	errFmt := "report feature has not been implemented yet, please check back later in a future release."
-	switch format {
-	case "json":
-		fmt.Println("json", errFmt)
-		os.Exit(0)
-	case "xml":
-		fmt.Println("xml", errFmt)
-		os.Exit(0)
-	case "txt":
-		report = frain.Text{
-			Data: &page,
-		}
-	default:
-		fmt.Println("bad file format specified:", format)
-		os.Exit(1)
-	}
-
-	if flagArgsLen < 2 {
+	if len(flagArgs) < 2 {
 		report.All(*quietFlag, *fullFlag)
 	} else {
-		report.Incidents(*quietFlag, *fullFlag)
+
+		switch subCommand {
+
+		case "incidents":
+			report.Incidents(*quietFlag, *fullFlag)
+
+		}
 	}
 }
 
-func versionInfo() {
+func setupVerInfo() {
 	// go build -ldflags "-X main.buildVersion=X.Y.Z"
 	if buildVersion == "" {
 		buildVersion = "<undefined>"
 	}
 	frain.Version = buildVersion
+}
+
+func parseFlagOptions() {
+	if *versionFlag {
+		frain.Init()
+		exit()
+	}
+
+	if *helpFlag {
+		frain.Init()
+		flag.Usage()
+		exit()
+	}
+
+	if *listFlag {
+		frain.Init()
+		listServices()
+		exit()
+	}
+
+	if len(*configFlag) != 0 {
+		fmt.Println("-c flag set. this feature has not yet been implemented. please check again later.")
+		exit()
+	}
 }
 
 func progress(c chan int) {
@@ -252,4 +198,84 @@ func listServices() {
 	for _, s := range sl {
 		fmt.Printf("\t%s\n", s)
 	}
+}
+
+func parseFlagParams(flagArgs []string) (string, time.Time, time.Time, error) {
+	subCommand := ""
+	startTime, _ := time.Parse("2006-01-02", "1970-01-01") // iso layout
+	endTime := time.Now()
+	flagArgsLen := len(flagArgs)
+
+	if flagArgsLen > 1 {
+		subCommand = flagArgs[1]
+		if _, ok := subCommands[subCommand]; !ok {
+			return subCommand, startTime, endTime, fmt.Errorf("unknown subcommand specified for %s: '%s'", flagArgs[0], flagArgs[1])
+		}
+	}
+
+	if flagArgsLen > 2 {
+		sTime, err := frain.CleanTimeArg(flagArgs[2])
+		if err != nil {
+			return subCommand, startTime, endTime, fmt.Errorf("start time error. %v", err)
+		}
+		startTime, err = time.Parse("2006-01-02", sTime)
+		if err != nil {
+			return subCommand, startTime, endTime, fmt.Errorf("bad format specified for start time: %v", flagArgs[2])
+		}
+	}
+
+	if flagArgsLen > 3 {
+		eTime, err := frain.CleanTimeArg(flagArgs[3])
+		if err != nil {
+			return subCommand, startTime, endTime, fmt.Errorf("end time error. %v", err)
+		}
+		endTime, err = time.Parse("2006-01-02", eTime)
+		if err != nil {
+			return subCommand, startTime, endTime, fmt.Errorf("bad format specified for end time: %v", flagArgs[3])
+		}
+	}
+
+	return subCommand, startTime, endTime, nil
+}
+
+func getReport(name, format string, startTime, endTime time.Time) (frain.Report, error) {
+	var report frain.Report
+	var page frain.Page
+	errFmt := "report feature has not been implemented yet, please check back later in a future release"
+
+	switch format {
+	case "json":
+		return nil, fmt.Errorf("json %v", errFmt)
+
+	case "xml":
+		return nil, fmt.Errorf("xml %v", errFmt)
+
+	case "txt":
+		report = frain.Text{
+			Data: &page,
+		}
+	}
+
+	var c = make(chan int)
+	go progress(c)
+
+	service, err := frain.GetService(name, startTime, endTime)
+	c <- 1
+	clear()
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.ToLower(service.Name) != name {
+		return nil, fmt.Errorf("'%s' is not a recognized service on frain (see \"frain --list\")", name)
+	}
+
+	page.Name, page.Service = name, service
+
+	return report, nil
+}
+
+func exit() {
+	// other cleanup tasks
+	os.Exit(0)
 }
